7e3103419c1689f6cf58c6a6f53cab961bcaffee
ReyRom <s03roman@mail.ru>
2022-12-24 17:57:52 +0300

    линейные вычисления

--- Math/Task1/Task1.cpp
+++ Math/Task1/Task1.cpp
@@ -2,12 +2,20 @@
 
 int main()
 {
+    //x^2+4x+1
     int x;
+    int y;
+    std::cin >> x;
     __asm {
-        mov eax, 2
-        mov ebx, 3
+        mov eax, x
+        mul eax
+        mov y, eax
+        mov ebx, 4
+        mov eax, x
         mul ebx
-        mov x, eax
+        add eax, y
+        add eax, 1
+        mov y, eax
     }
-    std::cout << x;
+    std::cout << y;
 }
