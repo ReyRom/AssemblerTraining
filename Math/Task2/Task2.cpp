78e8bd54606386962c71b11aa23bee43fb3fad43
ReyRom <s03roman@mail.ru>
2022-12-24 18:06:57 +0300

    cycle

---
+++ Math/Task2/Task2.cpp
@@ -0,0 +1,27 @@
+﻿// Task2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    //x!
+    int x;
+    int y;
+    std::cin >> x;
+    __asm {
+        mov ecx, x
+        mov eax, 1
+
+        cycle:
+        cmp ecx, 0
+        je quit
+        mul ecx
+        dec ecx
+        jmp cycle
+
+        quit:
+        mov y, eax
+    }
+    std::cout << y;
+}
